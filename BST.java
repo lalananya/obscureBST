b898b2e84e26a0420819045d692c6c30c95c246b
lalananya55 <lalananya55@gmail.com>
2023-04-05 15:46:07 +0530

    bst, deletion code updated

--- BST.java
+++ BST.java
@@ -34,6 +34,44 @@ public class BST {
         }
     }
 
+    void deleteElement(Node root, int element){
+        System.out.println("checking : " + root.id);
+        if(root != null){
+            if(root.id == element){
+                if(root.left == null && root.right == null){
+                    root = null;
+                    System.out.println("elememt deleted :" + root );
+                }
+                else if(root.left !=null && root.right == null){
+                    root.id = root.left.id;
+                    root.left = null;
+                    System.out.println("elememt deleted :" + root );
+                }
+                else if(root.right !=null && root.left == null){
+                    root.id = root.right.id;
+                    root.right = null;
+                    System.out.println("elememt deleted :" + root );
+                }
+                else {
+                    /* get the inOrder Successor */
+                    Node current = root;
+                    while(current.left != null){
+                        current = current.left;
+                    }
+                    root.id = current.id;
+                    current = null;
+                    System.out.println("elememt deleted :" + root );
+                    
+                }
+            }
+            if(root !=null){
+                if(root.id > element) deleteElement(root.left, element);
+                if(root.id < element) deleteElement(root.right, element);
+            }
+           
+        }   
+    }
+
     public static void main(String[] args) {
         BST obj = new BST();
         obj.insert(50);
@@ -42,6 +80,9 @@ public class BST {
         obj.insert(10);
         obj.insert(5);
         obj.insert(1);
+        obj.preOder(obj.root);
+        /* after insertion check the tree height, searching will take O(h), here self balancing comes into play */
+        obj.deleteElement(obj.root,1);
         obj.preOder(obj.root);
     }
 }
