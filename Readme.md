214fbc3eba4841eb0defb9821d7d2fdc747c2058
lalananya55 <lalananya55@gmail.com>
2023-04-06 18:56:52 +0530

    gibb code deleted, avl tree added : tree is balanced as per preorder, height code needs re check : wrong height calculated

---
+++ Readme.md
@@ -0,0 +1,140 @@
+            Trees are a little complex data structure, and there are many types as per use cases, 
+            tried to understand different trees on basis of where to use , their difference, 
+                                    and performing complex operations
+
+                                TO KNOW ABOUT DS, A LITTLE MORE 
+
+                                        *LANGUAGE : JAVA*
+                        *REFERENCE : GeeksForGeeks, YouTube, Upgrad, GOOGLE*
+
+*BST.java*
+
+Binary Search Tree
+ = atmost 2 subtress
+ = the left has elements lesser than the root, and right has greater
+ = OPERATION
+   insertion : normal rescursion
+   traversal : inOrder, preOrder, postOrder
+   deletion : there are cases ->    
+                            1. if the element to be deleted has no subtrees, assign the element to null
+                            2. if the element to be deleted has one subtree, assign that to the element
+                            3. if the element to be deleted has both, inOrder and go to the left most and replace
+    as we are dealing with subtrees here, replacing is the approach in case of deletion
+ = BFS & DFS usual
+
+*Node.java (common used)*
+ = Unique Id for the nodes, wrt class
+ = element refers Node class 
+
+*Creation.java (initial practice)*
+ = self
+
+*Self Balancing*
+
+ = as per elements, operations like insertion in a tree, the height of the tree could gradually increase, and accessing the element can lead upto complexity of O(height), worse case.
+ = Self Balancing trees can help us reduce the complexity and making operation on the trees easier.
+ = Types : AVL | Splay | AA | RedBlack | Scapegoat | BTree | Treaps
+ :: Tried to cover as much as I could find on google ::
+ = In any tree, rotations are of two types only, left or right now according to the use and type, we can do it multiple times
+   or one after the other, so this repo ignores the conventional name.
+ = We do not create the whole tree, then balance it, this is a wrong approach.
+
+ 1. AVL Tree
+    After every operation, we make sure the difference of the heights of left subtree and right subtree of any node, can not
+    be greater than 1.
+    Operation will use recursion, so think of Stack will writing the code
+    If -1 > H(left-Right) < 1, perform Rotations
+    
+    CASE 1 :
+
+    8
+        9   
+            10
+    
+    h(root8) = 0 - 2  = -2, left rotate it
+
+        9
+    8       10  , root changes to 9
+
+    if element 10 needs to be searched, in the first tree, we will get at the third comparision, 
+    but in the second tree, we will get at the second comparision,
+
+    reduces the checking of element, reduces time complexity
+    now , think about the large number of data.
+
+    CASE 2 :
+
+            8
+        6
+    5
+
+    h(root8) = 2 - 0 = 2, right rotate it
+
+        6
+    5       8, root changes to 6
+
+    CASE 3/4 : 
+
+                        8
+                6                 10   
+            5       7         9        15
+        4
+    3
+
+    let's say the last element inserted is 3, 
+    unbalaced nodes
+    h(root6) = 3 - 1 = 2, right rotate it, this is first parent root that is affected
+
+                            8
+                5                       10   
+            4       6               9        15
+        3               7
+
+    h(root8) = 3 - 2 = 1, no rotation needed
+
+    NOTE : whenever we insert the element, this is a recursion process so we will keep updating the height as well
+    and keep checking for the unbalanced node
+
+    CASE 5/6 : 
+
+                10
+            7
+              9
+
+    leftRotate by root.left
+
+            10
+        9
+    7
+
+    rightRotate it       9
+                     7       10
+
+    this is double rotation, in this case suppose we try to rotate right first
+    below would be the structure, this is still not balance
+    7
+        10
+       9     
+
+    rightRotate by root.left
+
+    7
+        9
+            10
+    now, left rotate it for         9
+                                7       10
+
+    Above are just the known cases, we are going to add some random exceptions as well, to make the rotations complex
+    still would want the tree to be balanced
+
+2. Splay Tree
+
+3. Red Black Tree
+
+4. AA Tree
+
+5. Treaps
+
+6. ScapeGoat Tree
+
+7. BTree
--- SelfBalancing.java
+++ SelfBalancing.java
@@ -1,345 +1,134 @@
-/* 
- * insertion and deletion operation in a tree can lead to skewed trees, so while performing these operations
- * we are know gping to balance with each operation, so the other operations like searching becomes easy
- * KEY : TO KEEP HEIGHT OF THE TREE AS SMALL AS POSSIBLE
- * Types and its basic idea: 
- *      AVL Tree - try balance the left and right subtree, where the difference should not be more than 1 for every node
- *                 key is balance as much as possible , accessing might take O(h)
- *      Red/Black Tree - 
- *      Splay Tree - bring the most recent accessed / inserted element to the root by performing rotations (splaying), searching
- *                   insertion , deletion O(logn), do not guarantee balanced, key is frequently accessed close to root    
+/* Self Balancing a BST, so the basic rule remains the same, 
+    nodes left to the root, smaller | 
+    nodes right to the root, greater |
  */
-
 class AVLTree {
-    Node root; /* refer - Node.java same package */
+    Node root;
 
     AVLTree(){
         root = null;
     }
 
-    /* Checking Balancing Factor on AVL Tree */
-
-    int height(Node obj){
-        return (obj !=null) ? obj.height : 0;
-    }
-
-    /* Checking Balancing Factor on AVL Tree */
-
-    int checkBalanceFactor(Node head){
-        int leftH = height(head.left);
-        int rightH = height(head.right);
-        return leftH - rightH;
-    }
-
-    void updateHeight(Node head){
-        int leftH = height(head.left);
-        int rightH = height(head.right);
-        head.height = 1 + Math.max(leftH, rightH);
-    }
     
-    /* Insertion on AVL Tree */
-
-    void insertElement(int element){
-        root = insertElementOperation(root, element);
+    int getHeight(Node node) {
+        if(node != null) return node.height;
+        return 0;
     }
 
-    Node insertElementOperation(Node head, int element){
-        if(head == null){
-            return new Node(element);
+    int balancingFactor(Node node){
+        if(node != null){
+            int left = getHeight(node.left);
+            int right = getHeight(node.right);
+            return left - right;
+        }
+        return 0;
+    }
+
+    void preOder(Node node){
+        if(node != null){
+            System.out.println("At : " + node.id + " with height : " + node.height);
+            preOder(node.left);
+            preOder(node.right);
+        }
+    }
+
+    void updateHeight(Node node){
+        if(node != null){
+            int left = getHeight(node.left);
+            int right = getHeight(node.right);
+            node.height = 1 + Math.max(left, right);
+        }
+    }   
+
+    Node leftRotate(Node node){
+        if(node != null){
+            Node newRoot = node.right;
+            Node saveNode = node.right.left;
+
+            newRoot.left = node;
+            node.right = saveNode;
+
+            updateHeight(newRoot);
+            updateHeight(node);
+
+            return newRoot;
+        }
+        return node;
+    }
+
+    Node rightRotate(Node node){
+        if(node != null){
+            Node newRoot = node.left;
+            Node saveNode = node.left.right;
+
+            newRoot.right = node;
+            node.left = saveNode;
+
+            updateHeight(newRoot);
+            updateHeight(node);
+
+            return newRoot;
+        }
+        return node;
+    }
+
+    Node insertElement(Node current, int id){ /* create a stack and check to understand */
+        if(current == null){
+            return new Node(id); /* searching for the null place to insert */
         }
         else {
-            if(element < head.id) {
-                head.left = insertElementOperation(head.left, element);
+            if(current.id < id) {
+                current.right = insertElement(current.right,id);
             }
-            else if(element > head.id) {
-                head.right = insertElementOperation(head.right, element);
+            else if(current.id > id) {
+                current.left = insertElement(current.left, id);
             }
         }
-
-        updateHeight(head);
-        int factor = checkBalanceFactor(head);
-
-        // System.out.println("-----------------------------------start-------------------------------------------------");
-        //     System.out.println("Factor-" + factor + "Element : " + element + "current pointer : " + head.id );
-        //     if(head.left != null) {
-        //         System.out.println("current's left : " + head.left.id);
-        //     }
-        //     if(head.right != null) {
-        //         System.out.println("current's right : " + head.right.id);
-        //     }
-        // System.out.println("------------------------------------end-------------------------------------------------");
-        
-        /* to fix rotation logic */
-        if(factor > 0 && head.left != null && head.left.id > element) {
-            rightRotate(head);
+        updateHeight(current);
+        int factor = balancingFactor(current);
+        if(factor < -1 && current.right.id < id) {
+            current = leftRotate(current);
         }
-        if(factor > 0 && head.left != null && head.left.id < element) {
-            head.left = leftRotate(head.left);
-            rightRotate(head);
+        if(factor < -1 && current.right.id > id){
+            current.right = rightRotate(current.right);
+            current = leftRotate(current);
         }
-        if(factor < 0 && head.right != null && head.right.id > element) {
-            leftRotate(head);
+        if(factor > 1 && current.left.id > id) {
+            current = rightRotate(current);
         }
-        if(factor < 0 && head.right != null && head.right.id < element) {
-            head.right = rightRotate(head.right);
-            leftRotate(head);
+        if(factor > 1 && current.left.id < id){
+            current.left = leftRotate(current.left);
+            current = rightRotate(current);
         }
-
-        return head;
+        return current; /* recursion keeps returning , at last it gets back to the starting point  */
     }
-
-    void preOder(Node head){
-        if(head !=null){
-            System.out.println("HEAD -> " + head.id + "Height ->" + head.height);
-            preOder(head.left);
-            preOder(head.right);
-        }
-    }
-
-    /* Deletion on AVL Tree :
-     * after every deletion , balance the tree
-     */
 
     void deleteElement(){
 
     }
 
-    /* Searching on AVL Tree:
-     * try searching without balancing (BST.java), and with balancing
-     */
+    void findElement(){
 
-    void searchElement(Node head, int element){
-        if(head != null){
-            if(head.id == element) {
-                System.out.println("found");
-            }
-            else {
-                if(head.id < element) searchElement(head.right, element);
-                else searchElement(head.left, element);
-            }
-        }
     }
 
-    /* Rotations on AVL Tree
-     * rotate and check balance factor, then re-check : KEY
-     */
+    void start(){
+        root = insertElement(root,50);
+        root = insertElement(root,40);
+        root = insertElement(root,60);
+        root = insertElement(root,70);
+        root = insertElement(root,65);
+        root = insertElement(root,30);
+        root = insertElement(root,20);
+        root = insertElement(root,10);
+        System.out.println("Current Root" + root.id);
+        preOder(root);
 
-    Node leftRotate(Node head){
-        if(head != null) {
-            Node hr = head.right;
-            Node hrf = hr!=null ? hr.left : null;
-
-            if(hr != null && head !=null){
-                hr.left = head;
-                head.right = hrf;
-                updateHeight(head);
-                updateHeight(hr);
-                return hr;   
-            }
-            return head;
-        }
-        return head;
-    }
-
-    Node rightRotate(Node head){
-        if(head != null){
-            Node hr = head.left;
-            Node hrf = hr!=null ? hr.right : null;
-
-            if(hr!= null && head !=null){
-                hr.right = head;
-                head.left = hrf;
-                updateHeight(head);
-                updateHeight(hr);
-                return hr;
-            } 
-            
-            return head;
-        }
-        return head;
-    }
-    /* ---------------------- */
-
-    void avl(AVLTree avlObj) {
-        avlObj.insertElement(8);
-        avlObj.insertElement(7);
-        avlObj.insertElement(6);
-        avlObj.insertElement(9);
-        avlObj.insertElement(2);
-        avlObj.insertElement(3);
-        avlObj.insertElement(10);
-        avlObj.insertElement(1);
-        avlObj.insertElement(12);
-        avlObj.preOder(avlObj.root);
-        // avlObj.searchElement(avlObj.root, 123);
     }
 }
-
-class Splay {
-    
-    Node root;
-
-    Splay(){
-        root = null;
-    }
-
-    /* all functions not needed , added just to understand */
-    Node rotate(Node head, int check){
-        /* 0 : zig rotation - single right rotation
-         * 1 : zag rotation - single left rotation
-         * 2 : zig/ zag rotation - right rotate then left
-         * 3 : zag/ zig rotation - left rotate the right
-         * 4 : zig/ zig rotation - double right rotation
-         * 5 : zag/ zag rotation - double left rotation
-         */
-        switch(check){
-            case 0 : return zigRotate(head);
-            case 1 : return zagRotate(head);
-            case 2 : return zigzagRotate(head);
-            case 3 : return zagzigRotate(head);
-            case 4 : return zigzigRotate(head);
-            case 5 : return zagzagRotate(head);
-            default :  return head;
-        }
-    }
-
-    Node zagRotate(Node head) {
-        Node nextHead = head.right;
-        Node leftRight = nextHead.left;
-
-        nextHead.left = head;
-        head.right = leftRight;
-        
-        return nextHead;
-    }
-
-    Node zigRotate(Node head) {
-        Node nextHead = head.left;
-        Node leftRight = nextHead.right;
-
-        nextHead.right = head;
-        head.left = leftRight;
-        
-        return nextHead;
-    }
-
-    Node zagzagRotate(Node head) {
-        head = zagRotate(head);
-        head = zagRotate(head);
-        return head;
-    }
-
-    Node zigzigRotate(Node head) {
-        head = zigRotate(head);
-        head = zigRotate(head);
-        return head;
-    }
-
-    Node zagzigRotate(Node head) {
-        head = zagRotate(head);
-        head = zigRotate(head);
-        return head;
-    }
-    
-    Node zigzagRotate(Node head) {
-        head = zigRotate(head);
-        head = zagRotate(head);
-        return head;
-    }
-    
-    /* all functions not needed , added just to understand */
-
-    Node performSplay(Node head, int id){
-        if(head == null || head.id == id) {
-            return head; /* the element is either null or present at head */
-        }
-
-        if(head.id > id) { /* element lies in left */
-            
-            if(head.left == null ) return head;
-            if(head.left.id > id) {
-                    head.left.left = performSplay(head.left.left,id);
-                    head = zigRotate(head);
-            }
-            else if(head.left.id < id){
-                    head.left.right = performSplay(head.left.right, id);
-                    if(head.left.right !=null){
-                        head.left = zagRotate(head.left);
-                    }
-            }
-            return head.left == null ? head : zigRotate(head);
-        }
-        else {
-            if(head.right == null) return head;
-            if(head.right.id > id) {
-                head.right.left = performSplay(head.right.left, id);
-                if(head.right.left !=null) {
-                    root.right = zigRotate(head.right);
-                }
-            }
-            else if( head.right.id  < id) {
-                head.right.right = performSplay(head.right.right, id);
-                head = zagRotate(head);
-            }
-            return head.right == null ? head : zagRotate(head); 
-        }
-    }
-
-    void insertElement(int id){
-        root = insertOperation(root, id);
-    }
-
-    Node insertOperation(Node head, int id){
-        if(head == null){
-            return new Node(id);
-        }
-
-        head = performSplay(head, id); /* to bring the closest leaf to node */
-
-        if(head.id == id) return head;
-        Node newnode = new Node(id);
-
-        if(head.id > id) {
-            newnode.left = head;
-            newnode.right = head.right;
-            head.right = null;
-        }
-        else {
-            newnode.left = newnode;
-            newnode.right = head.right;
-            head.right = null;
-        }
-        return newnode;
-    }
-
-    void preOder(Node root){
-        if(root != null){
-            System.out.println("Element : " + root.id + " Height : " + root.height);
-            preOder(root.left);
-            preOder(root.right);
-        }
-    }
-
-    void splay(Splay splay){
-        splay.insertElement(50);
-        splay.insertElement(60);
-        splay.insertElement(55);
-        splay.insertElement(20);
-        splay.insertElement(10);
-        splay.insertElement(25);
-        splay.insertElement(65);
-        splay.preOder(splay.root);
-    }
-
-}
-
 public class SelfBalancing {
     
     static void initiate(){
-        // AVLTree avlObj = new AVLTree();
-        // avlObj.avl(avlObj);
-        Splay splayObj = new Splay();
-        splayObj.splay(splayObj);
+        new AVLTree().start();
     }
     public static void main(String[] args) {
         initiate();
